102b3863e9ef1094550f1b5a6e30ab89ffbba0f6
taewok <taewok5163@naver.com>
2023-02-28 17:49:22 +0900

    초기 setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import Component from "./component/Component.tsx";
 
 function App() {
+  // useEffect(() => {
+  //   axios.get(`	https://api.adviceslip.com/advice`).then((res) => {
+  //     console.log(res.data);
+  //   });
+  // });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Component>
+      </Component>
     </div>
   );
 }
---
+++ src/style/theme.js
@@ -0,0 +1,14 @@
+export const lightTheme = {
+  backgroundColor: "#ffffff",
+};
+
+export const darkTheme = {
+  backgroundColor: "#000000",
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;

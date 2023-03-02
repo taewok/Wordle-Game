a248f64d7cf7c93fed135c0590e0606931b66484
taewok <taewok5163@naver.com>
2023-03-02 16:39:54 +0900

    feat: Home 화면 ui 개발 완료

---
+++ src/page/Home.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+
+const Home = () => {
+  return (
+    <Container>
+      <Title>
+        <p>영어 명언으로 단어 퀴즈</p>
+        <span>Word quiz with English sayings</span>
+        <StartBtn>
+          <Link to={`/quiz`}>시작하기</Link>
+        </StartBtn>
+      </Title>
+    </Container>
+  );
+};
+
+const closeUp = keyframes`
+    0%{
+        font-size: 10rem;
+        transform: translate(0,120%);
+    }
+    100%{
+        font-size: 4rem;
+        transform: translate(0,-20%);
+    }
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+const Title = styled.h1`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #f88d00;
+  animation: ${closeUp} 1s normal forwards;
+  span {
+    font-size: 2rem;
+    color: #747474;
+  }
+`;
+const StartBtn = styled.button`
+  margin-top: 10px;
+  padding: 10px 45px;
+  background-color: transparent;
+  border-radius: 20px;
+  font-size: 1rem;
+  transition: all 0.3s;
+  cursor: pointer;
+  &:hover {
+    background-color: #fcf9be;
+    font-weight: bold;
+  }
+  a {
+    color: black;
+  }
+`;
+
+export default Home;

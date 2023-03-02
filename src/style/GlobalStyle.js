9e7d9197042c3b301f8f4ac7c4127da15d5bbf88
taewok <taewok5163@naver.com>
2023-03-02 16:38:54 +0900

    style: font url 적용

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,12 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'BMJUA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
     * {
         margin:0;
         padding:0;
     }
     body {
+        display: flex;
+        justify-content: center;
         height:100vh;
+        background-color: #E8F3D6;
+        font-family: "BMJUA";
     }
     a {
         text-decoration: none;

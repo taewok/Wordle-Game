e609be6607fa142811f9c3f115b8f05c972a0396
taewok <taewok5163@naver.com>
2023-03-15 19:42:34 +0900

    feat: 도전 횟수를 state로 관리

--- src/page/Quiz.tsx
+++ src/page/Quiz.tsx
@@ -8,7 +8,9 @@ import Result from "../component/Result";
 
 const Quiz = () => {
   const [word, setWord] = useState<string>("");
+  //게임에 승패에 따라 마지막에 뜨는 정보창이 달라진다.
   const [result, setResult] = useState<boolean>(false);
+  const [challengeCount, setChallengeCount] = useState<number>(0);
 
   //단어를 랜덤으로 뽑아 word에 담는다
   useEffect(() => {
@@ -36,7 +38,7 @@ const Quiz = () => {
   return (
     <Container>
       <Alert className="alert">알파벳을 모두 입력해주세요</Alert>
-      <Result result={result}/>
+      <Result result={result} challengeCount={challengeCount} word={word} />
       <Wrap>
         <Question />
         <Answer />
@@ -46,7 +48,11 @@ const Quiz = () => {
         <Answer />
         <Answer />
       </Wrap>
-      <KeyBoard word={word} setResult={setResult}/>
+      <KeyBoard
+        word={word}
+        setResult={setResult}
+        setChallengeCount={setChallengeCount}
+      />
     </Container>
   );
 };
@@ -83,6 +89,7 @@ const Wrap = styled.div`
 const Alert = styled.div`
   opacity: 0;
   position: absolute;
+  z-index: 999;
   top: 40%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -91,7 +98,7 @@ const Alert = styled.div`
   align-items: center;
   width: 300px;
   height: 50px;
-  background-color: #c0c0c0da;
+  background-color: #e7e7e7da;
   &.active {
     animation: ${AlertAnimated} 1.5s;
   }
